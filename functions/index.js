6454bd179ba356128e1b85d17c6c4ea668ec722d
Jeniffer Giraldo <tris@Jeniffers-MacBook-Air-2.local>
2023-07-05 11:11:09 -0400

    ADDED get and add tasks

---
+++ functions/index.js
@@ -0,0 +1,17 @@
+import { onRequest } from "firebase-functions/v2/https";
+import express  from "express";
+import cors from "cors";
+import { getTasks, addTask } from "./src/tasks.js";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+//routes:
+app.get("/tasks/:uid", getTasks);
+app.post("/tasks/:uid", addTask);
+
+export const api = onRequest(app); // exports the cloud function
+
+
+
---
+++ functions/src/tasks.js
@@ -0,0 +1,29 @@
+import { FieldValue } from "firebase-admin/firestore"; // add firestore time stamp
+import db from "./dbConnect.js";
+
+const coll = db.collection("tasks");
+
+export async function getTasks(req, res) {
+    const { uid } = req.params;
+    // get all tasks by user:
+    const tasks = await coll.where('uid', '==', uid).get();
+    // arrange tasks in an array:
+    const taskArray = tasks.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+    res.send(taskArray);
+}
+
+export async function addTask(req, res) {
+    const { title, uid } = req.body;
+   if (!title || !uid) {
+    res.status(401).send({ success: false, message: 'Not a valid request'});
+    return;
+   }
+   const newTask = {
+    title, 
+    uid, 
+    done: false,
+    createdAt: FieldValue.serverTimestamp(),
+   }
+   await coll.add(newTask);
+   getTasks(req, res);
+}
